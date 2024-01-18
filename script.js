ee9b162979175a1b787b65d9ad406e63590889d5
Nauclhlt <111179210+Nauclhlt@users.noreply.github.com>
2024-01-18 21:50:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+var elapsedTime = 0;
+var isPaused = false;
+var isStarted = false;
+
+window.onload = function () {
+    setInterval(onTimer, 1000);
+};
+
+function startButtonClicked()
+{
+    if (!isStarted)
+    {
+        isStarted = true;
+        isPaused = false;
+        elapsedTime = 0;
+        updateTimeLabel();
+    }
+    else
+    {
+        isPaused = !isPaused;
+        updateTimeLabel();
+    }
+
+    updateStartButton();
+}
+
+function resetButtonClicked()
+{
+    isStarted = false;
+    isPaused = false;
+    elapsedTime = 0;
+
+    updateTimeLabel();
+    updateStartButton();
+}
+
+function onTimer()
+{
+    if (isStarted)
+    {
+        if (!isPaused)
+        {
+            elapsedTime++;
+
+            if (elapsedTime == 3 * 60)
+            {
+                playBell();
+            }
+        }
+    }
+
+    updateTimeLabel();
+}
+
+function playBell()
+{
+    let audio = document.getElementById("bellAudio");
+    audio.play();
+}
+
+function updateTimeLabel()
+{
+    let label = document.getElementById("time-label");
+    
+    let minutes = padZero(Math.floor(elapsedTime / 60), 2);
+    let seconds = padZero(elapsedTime % 60, 2);
+
+    label.textContent = minutes + ":" + seconds;
+}
+
+function updateStartButton()
+{
+    let button = document.getElementById("start-button");
+    
+    if (isStarted)
+    {
+        if (isPaused)
+        {
+            button.textContent = "再開";
+        }
+        else
+        {
+            button.textContent = "停止";
+        }
+    }
+    else
+    {
+        button.textContent = "開始";
+    }
+}
+
+function padZero(number, length){
+	return ( Array(length).join('0') + number ).slice( -length );
+}
